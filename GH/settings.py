6ff35d293b3385db7cb67282acce72597e81a70d
10mi3 <58175007+10mi3@users.noreply.github.com>
2022-12-31 16:54:45 +0100

    update setting

--- GH/settings.py
+++ GH/settings.py
@@ -15,7 +15,7 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 from datetime import timedelta
 
-# from decouple import config
+from decouple import config
 
 from pathlib import Path
 
@@ -24,13 +24,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'q6ps3l++p9if3@q!x-=q2cp728ozxrcaw)sk$(gf15*o81**&r'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 # Application definition
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', [], cast=lambda v: [s.strip() for s in v.split(',')])
 
 # runserver.default_port = '8015'
 # runserver.default_addr = '127.0.0.1'
@@ -60,6 +60,7 @@ APPEND_SLASH = False
 
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
